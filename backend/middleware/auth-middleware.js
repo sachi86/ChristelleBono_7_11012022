33fb01b9e184d93146418605e9a27c7cf9e98da2
sachi86 <sachi8676@gmail.com>
2022-01-28 13:50:55 +0100

    create multer and auth middlewares

---
+++ backend/middleware/auth-middleware.js
@@ -0,0 +1,29 @@
+//Call the module of jsonwebtoken for token management
+const jwt = require('jsonwebtoken');
+
+//Call to dotenv 
+const dotenv = require('dotenv');
+
+//Call to function of dotenv config
+dotenv.config({ path: './.env' });
+
+
+//Allows authentication verification
+module.exports = (req, res, next) => {
+  try {
+    // const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers["x-access-token"];
+    console.log(req.headers);//Get the token in the request header
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN);//To décode the token with key
+    const user_id = decodedToken.user_id;//Get the user id in the decode token 
+    if (req.body.user_id && req.body.user_id !== user_id) {
+      throw 'Invalid user ID';
+    } else {              //Check that the user id of the token is the same as that of the request
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')// response error unauthorized 
+    });
+  }
+};
---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,26 @@
+//Call the module multer for use
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg':'jpg',
+    'image/png' : 'png',
+    'image/bmp' : 'bmp',
+    'image/gif' : 'gif',
+    'image/webp' : 'webp'
+};
+
+//This is a configuration object for multer
+const storage = multer.diskStorage({//this function is used to store on disk 
+    destination: (req, file, callback) => {
+        callback(null,'images');//To know in which folder the image should be saved
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');//For replace spaces with underscors
+        const extension = MIME_TYPES[file.mimetype];// For to manage the image's extension
+        callback(null, name + Date.now() + '.' + extension);//For final name file whitch save in folder
+    }
+});
+
+//Call multer to passed the object storage say 'save with multer params' and to call method single for say 'is a unique file' and passed image for say 'is a image file
+module.exports = multer({storage: storage}).single('image');
